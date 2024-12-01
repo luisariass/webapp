b186f6960e237c5cb7cd51dbafd66fe321e040e7
luisariass <ariasl@utb.edu.co>
2024-11-30 22:29:24 -0500

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+var constructor = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
